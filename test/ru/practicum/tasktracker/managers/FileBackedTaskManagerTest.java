8dfcb2902bd0f2c86c6a00d73e15f8f3f6e2884d
Сергей <master-fores@yandex.ru>
2024-04-04 17:24:06 +0300

    Удалены тесты сохранения и загрузки

--- test/ru/practicum/tasktracker/managers/FileBackedTaskManagerTest.java
+++ test/ru/practicum/tasktracker/managers/FileBackedTaskManagerTest.java
@@ -1,35 +1,10 @@
 package ru.practicum.tasktracker.managers;
 
-import org.junit.jupiter.api.Test;
-import ru.practicum.tasktracker.exceptions.ManagerLoadException;
-import ru.practicum.tasktracker.exceptions.ManagerSaveException;
-import ru.practicum.tasktracker.models.Task;
-
 import java.io.File;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class FileBackedTaskManagerTest extends TaskManagerTest<FileBackedTaskManager> {
     @Override
     protected FileBackedTaskManager createTaskManager() {
         return new FileBackedTaskManager(new File("resources\\test.csv"));
-    }
-
-    @Test
-    void testSaveToFile() {
-        File invalidFile = new File("invalid\\path\\to\\file.pdf");
-        FileBackedTaskManager manager = new FileBackedTaskManager(invalidFile);
-        Task task = new Task("111", "222");
-        assertThrows(ManagerSaveException.class, () -> {
-            manager.createTask(task);
-        }, "Сохранение не должно происходить");
-    }
-
-    @Test
-    void testLoadFromFile() {
-        File invalidFile = new File("invalid\\path\\to\\file.pdf");
-        assertThrows(ManagerLoadException.class, () -> {
-            FileBackedTaskManager.loadFromFile(invalidFile);
-        }, "Загрузка не должна происходить");
     }
 }
